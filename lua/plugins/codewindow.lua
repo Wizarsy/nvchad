5d77054c8f29ffe8b8283d999dfcca1a6d9f3b04
Wizarsy <filipemadruga@outlook.com>
2024-12-03 11:36:48 -0300

    removed plugins

--- lua/plugins/codewindow.lua
+++ lua/plugins/codewindow.lua
@@ -1,4 +1,22 @@
 return {
   "gorbit99/codewindow.nvim",
-  opts = {},
+  event = { "BufEnter" },
+  opts = {
+    window_border = "rounded",
+  },
+  config = function(_, opts)
+    local codewindow = require "codewindow"
+    codewindow.setup(opts)
+    codewindow.apply_default_keybinds()
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      callback = function()
+        codewindow.open_minimap()
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "BufDelete" }, {
+      callback = function()
+        codewindow.close_minimap()
+      end,
+    })
+  end,
 }
